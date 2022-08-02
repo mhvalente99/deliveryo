582669b38f92ef47d7728dd0ba4ae1a5aac83126
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-08-02 09:27:35 -0300

    feat: add route in clients to authenticate client

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController();
 
 routes.post("/client", createClientController.handle);
+routes.post("client/authenticate", authenticateClientController.handle);
 
 export { routes };
