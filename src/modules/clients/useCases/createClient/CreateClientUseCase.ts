ee0d87a7e85dd19873024cc81253a22d9bbf81ee
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-08-02 09:08:20 -0300

    feat: create use case to create a new client

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,32 @@
+import { prisma } from "../../../../database/prismaClient";
+import { hash } from "bcrypt";
+
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+export class CreateClientUseCase {
+  async execute({ username, password }: ICreateClient) {
+    const clientExists = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive"
+        }
+      }
+    });
+
+    if (clientExists) {
+      throw new Error("Client already exists");
+    }
+
+    const hashPassword = await hash(password, 10);
+
+    await prisma.clients.create({
+      data: {
+        username,
+        password: hashPassword
+      }
+    })
+  }
+}
