93700b94d30cb9e4ddd877fe99a0168e7906215e
matheus.valente <matheus.valente@tecnospeed.com.br>
2022-08-02 22:38:58 -0300

    feat: create route deliveryman to create a new deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/CreateDeliverymanController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 
+const createDeliverymanController = new CreateDeliverymanController();
+
 routes.post("/client", createClientController.handle);
-routes.post("client/authenticate", authenticateClientController.handle);
+routes.post("/client/authenticate", authenticateClientController.handle);
+
+routes.post("/deliveryman", createDeliverymanController.handle);
 
 export { routes };
